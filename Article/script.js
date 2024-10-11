8ddfc5b112ee3f1a657e1d5c2a5261cebeedde17
jsanthea_ <juliasantheamontejo@gmail.com>
2024-10-11 09:31:09 +0800

    script.js

--- Article/script.js
+++ Article/script.js
@@ -2,10 +2,8 @@ document.getElementById('explore-button').addEventListener('click', function() {
     document.getElementById('hidden-sections').style.display = 'block';
 });
 
-// Get all gallery images
 const galleryImages = document.querySelectorAll('.gallery-image');
 
-// Add event listener to each gallery image
 galleryImages.forEach((image) => {
     image.addEventListener('click', function() {
         // Get the image source and content
@@ -27,7 +25,6 @@ galleryImages.forEach((image) => {
     });
 });
 
-// Add event listener to the lightbox back button
 document.getElementById('lightbox-back-button').addEventListener('click', function() {
     // Hide the lightbox
     document.getElementById('lightbox-container').style.display = 'none';
